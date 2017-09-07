53fa42631c22ff37a139639074de513e4e0c2934
Hlengi <u14077893@tuks.co.za>
2017-09-07 17:43:39 +0200

    Initial Commit

---
+++ PromotionsApp/src/main/java/com/hlengi/cos731/bellisimo/PromotionsRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hlengi.cos731.bellisimo;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author hjita
+ */
+public interface PromotionsRepository extends CrudRepository<Promotion, Long> 
+{
+    List<Promotion> findByProduct(String prodName);
+}
